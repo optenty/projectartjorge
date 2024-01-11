376a7776e0496c9122810fb10add1e45f2e74346
Jorge <jcp40500@gmail.com>
2024-01-11 18:43:28 +0100

    11/01/2024 classe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './art/pages/home-page/home-page.component';
+import {ArtModule} from "./art/art.module";
+import { PaintingListComponent } from './components/painting-list/painting-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaintingListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ArtModule
   ],
   providers: [],
   bootstrap: [AppComponent]
