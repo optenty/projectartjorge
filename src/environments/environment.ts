351322db88e836b69635a445b8e3b7a992c51130
optenty <45008583+optenty@users.noreply.github.com>
2024-02-11 00:05:33 +0100

    Delete

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment={
-  production:false,
-  supabaseUrl: 'https://qoqbovdljzwvtbaobeml.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvcWJvdmRsanp3dnRiYW9iZW1sIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwNTE4MzA0MCwiZXhwIjoyMDIwNzU5MDQwfQ.HyaPXAkt8YlNaPqWv-1wiIHAlaF6VERVZloF06k_0AY'
-}
